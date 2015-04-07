c9e21934339dfdd368d8d377a8559db7df68657e
eduardobc <eduardobc.88@gmail.com>
2015-04-07 17:46:33 -0500

    Quadratin: SplashScreen,Intro

---
+++ Quadratin/app/src/main/java/g4a/quadratin/mx/quadratin/activity_quadratin_splash_screen.java
@@ -0,0 +1,49 @@
+package g4a.quadratin.mx.quadratin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ProgressBar;
+
+/**
+ * Created by eduardo on 4/7/15.
+ */
+public class activity_quadratin_splash_screen  extends ActionBarActivity {
+    public static final int secconds = 4;
+    public static final int milsecconds = secconds*1000;
+    private ProgressBar q_sc_progressbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quadratin_splash_screen);
+
+        q_sc_progressbar = (ProgressBar)findViewById(R.id.q_sc_progressbar);
+        quadratin_start_progressbar();
+    }
+
+
+    /* ========== start progressbar ========== */
+    public void quadratin_start_progressbar() {
+        new CountDownTimer(milsecconds,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //Log.d("onTick", "millisUntilFinished = "+millisUntilFinished);
+                q_sc_progressbar.setProgress(set_progress(millisUntilFinished));
+            }
+
+            @Override
+            public void onFinish() {
+                Intent quadratin_activity_intro = new Intent(activity_quadratin_splash_screen.this,activity_quadratin_intro.class);
+                startActivity(quadratin_activity_intro);
+                finish();
+            }
+        }.start();
+    }
+
+    public int set_progress(long milisecconds) {
+        return (int)((milsecconds-milisecconds)/1000);
+    }
+    /* ========== end progressbar ========== */
+}
